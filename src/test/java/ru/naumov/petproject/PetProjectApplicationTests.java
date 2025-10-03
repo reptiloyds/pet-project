1457654f74496b8dc53d15675031615ef75f6742
Ilya Naumov <hairon20152003@mail.ru>
2025-10-03 18:09:49 +0300

    Init

---
+++ src/test/java/ru/naumov/petproject/PetProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.naumov.petproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PetProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
